d5297fc246e0d8f4910b393270af9ea922679ffa
ChangRongKun <cccrk123@163.com>
2020-08-26 15:45:05 +0800

    登录组件布局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
