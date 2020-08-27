8758f763e2d4dc8429ff6786619d209d90a9406e
ChangRongKun <cccrk123@163.com>
2020-08-27 11:13:47 +0800

    退出功能的实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Home组件</div>
+  <div>
+    <el-button type="info" @click="logout">
+      退出
+    </el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+  /**
+   * 退出登录
+   */
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
