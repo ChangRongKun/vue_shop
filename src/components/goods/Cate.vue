b52da6d6ddf1500246824f7c0044e404202a85ad
ChangRongKun <cccrk123@163.com>
2020-09-14 10:20:03 +0800

    请求商品分类数据并使用vue-table-with-tree-grid插件展示

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -16,7 +16,27 @@
         </el-col>
       </el-row>
       <!-- 表格 -->
+      <!-- data   表格各行的数据
+           columns    表格各列的配置
+           selection-type  是否为多选类型表格
+           expand-type    是否为展开行类型表格
+           show-index   是否显示数据索引
+           index-text   数据索引名称
+           border   是否显示纵向边框
+           stripe   是否显示间隔斑马纹
+           show-row-hover   鼠标悬停时，是否高亮当前行
+       -->
+      <tree-table
+        :data="cateList"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        index-text="#"
+        border
+        :show-row-hover="false">
 
+      </tree-table>
       <!-- 分页区域 -->
 
     </el-card>
@@ -25,7 +45,47 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+    // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表
+      cateList: [],
+      // 总条数
+      total: 0,
+      // 为 table 指定列的定义
+      columns: [
+        {
+          // 列标题名称
+          label: '分类名称',
+          // 对应列内容的属性名
+          prop: 'cat_name'
+        }
+      ]
+    }
+  },
+  created () {
+    this.getCateList()
+  },
+  methods: {
+    /**
+     * 获取商品分类信息
+     */
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败！')
+      }
+      // 把数据列表赋值给cateList
+      this.cateList = res.data.result
+      // 为总数居条数赋值
+      this.total = res.data.total
+    }
+  }
 }
 </script>
 
