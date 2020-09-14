1b12c894bf4cc8598ac7dfe7209daac3cad4826c
ChangRongKun <cccrk123@163.com>
2020-09-14 09:38:53 +0800

    商品分类的基本页面布局

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
 
-    <h3>商品分类组件</h3>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <!-- 添加区域 -->
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+
+      <!-- 分页区域 -->
+
+    </el-card>
   </div>
 </template>
 
