dbf6254794a634c272db8b11a169c9dfe04110a7
ChangRongKun <cccrk123@163.com>
2020-09-15 15:31:14 +0800

    获取商品分类列表、并渲染对应的UI页面

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -20,6 +20,18 @@
         <el-col>
           <span>选择商品分类：</span>
           <!-- 选择商品分类的级联选择框 -->
+          <!--
+            options   用来指定数据源
+            props   用来指定配置对象
+            clearable   是否支持清空选项
+          -->
+          <el-cascader v-model="selectedCateKeys"
+                       :options="cateList"
+                       :props="cateProps"
+                       @change="cateChange"
+                       clearable
+                       >
+          </el-cascader>
         </el-col>
       </el-row>
     </el-card>
@@ -30,14 +42,46 @@
 export default {
   data () {
     return {
-
+      // 商品分类列表
+      cateList: [],
+      // 级联选择框的配置对象
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        // 次级菜单的展开方式
+        expandTrigger: 'hover'
+      },
+      // 级联选择框双向绑定到的数组
+      selectedCateKeys: []
     }
   },
   created () {
-
+    this.getCateList()
   },
   methods: {
-
+    /**
+     * 获取所有的商品分类列表
+     */
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败！')
+      }
+      this.cateList = res.data
+    },
+    /**
+     * 级联选择框选中项变化会触发的函数
+     */
+    cateChange () {
+      // 证明选中的不是三级分类
+      if (this.selectedCateKeys.length < 3) {
+        this.selectedCateKeys = []
+        return
+      }
+      // 证明选中的是三级分类
+      console.log(this.selectedCateKeys)
+    }
   }
 }
 </script>
