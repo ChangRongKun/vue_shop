bcab23ed0953a37bef360ec88581e237e1f30757
ChangRongKun <cccrk123@163.com>
2020-09-15 15:05:54 +0800

    通过路由加载分类参数组件

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>商品参数</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
