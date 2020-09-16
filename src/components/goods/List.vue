ad0021518f638e121da425516574d3acaecade1d
ChangRongKun <cccrk123@163.com>
2020-09-16 16:35:59 +0800

    获取商品列表数据、并渲染到页面

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -9,6 +9,7 @@
 
     <!-- 卡片视图区域 -->
     <el-card>
+      <!-- 头部搜索、添加区域 -->
       <el-row :gutter="20">
         <el-col :span="8">
           <!-- 搜索的文本输入框 -->
@@ -21,6 +22,25 @@
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
+
+      <!-- table表格区域 -->
+      <el-table :data="goodsList"
+                border
+                stripe>
+        <!-- 索引列 -->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="80px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time"
+        width="140px"></el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template>
+            <el-button type="primary" size="mini" icon=el-icon-edit>编辑</el-button>
+            <el-button type="danger" size="mini" icon=el-icon-delete>删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -29,14 +49,35 @@
 export default {
   data () {
     return {
-
+      // 查询参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 获取的商品列表
+      goodsList: [],
+      // 总数据条数
+      total: 0
     }
   },
   created () {
-
+    this.getGoodsList()
   },
   methods: {
+    /**
+     * 根据分页获取的商品列表
+     */
+    async getGoodsList () {
+      const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
 
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数列表失败！')
+      }
+
+      this.goodsList = res.data.goods
+      this.total = res.data.total
+    }
   }
 }
 </script>
