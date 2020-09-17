4b70f2411f125b096143f0cd847d30b9691c8945
ChangRongKun <cccrk123@163.com>
2020-09-17 10:30:57 +0800

    绘制基本信息 面板、获取商品分类信息、并展示

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -30,21 +30,63 @@
         <el-step title="完成"></el-step>
       </el-steps>
 
-      <!-- tab栏区域 -->
-      <el-tabs v-model="activeIndex"
-               :tab-position="'left'"
-               style="height: 200px;">
-        <el-tab-pane label="基本信息"
-                     name="0">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数"
-                     name="1">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性"
-                     name="2">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片"
-                     name="3">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容"
-                     name="4">商品内容</el-tab-pane>
-      </el-tabs>
+      <el-form :model="addForm"
+               :rules="addFormRules"
+               ref="addFormRef"
+               label-width="100px"
+               label-position="top">
+        <!-- tab栏区域 -->
+        <el-tabs v-model="activeIndex"
+                 :tab-position="'left'">
+
+          <!-- 基本信息        -->
+          <el-tab-pane label="基本信息"
+                       name="0">
+            <el-form-item label="商品名称"
+                          prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格"
+                          prop="goods_price">
+              <el-input v-model="addForm.goods_price"
+                        type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量"
+                          prop="goods_number">
+              <el-input v-model="addForm.goods_number"
+                        type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量"
+                          prop="goods_weight">
+              <el-input v-model="addForm.goods_weight"
+                        type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类"
+                          prop="goods_cat">
+              <el-cascader v-model="addForm.goods_cat"
+                           :options="cateList"
+                           :props="cateProps"
+                           @change="cateHandleChange"></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+
+          <!-- 商品参数 -->
+          <el-tab-pane label="商品参数"
+                       name="1">商品参数</el-tab-pane>
+
+          <!-- 商品属性 -->
+          <el-tab-pane label="商品属性"
+                       name="2">商品属性</el-tab-pane>
+
+          <!-- 商品图片 -->
+          <el-tab-pane label="商品图片"
+                       name="3">商品图片</el-tab-pane>
+
+          <!-- 商品内容 -->
+          <el-tab-pane label="商品内容"
+                       name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -53,14 +95,79 @@
 export default {
   data () {
     return {
-      activeIndex: '0'
+      activeIndex: '0',
+      // 添加商品的表单数据对象
+      addForm: {
+        // 商品名称
+        goods_name: '',
+        // 商品价格
+        goods_price: 0,
+        // 商品数量
+        goods_number: 0,
+        // 商品重量
+        goods_weight: 0,
+        // 商品所属的分类数组
+        goods_cat: []
+      },
+      // 添加商品表单的验证规则
+      addFormRules: {
+        // 商品名称的校验规则
+        goods_name: [
+          { required: true, message: '请输入商品名称', triggger: 'blur' }
+        ],
+        // 商品价格的校验规则
+        goods_price: [
+          { required: true, message: '请输入商品价格', triggger: 'blur' }
+        ],
+        // 商品数量的校验规则
+        goods_number: [
+          { required: true, message: '请输入商品数量', triggger: 'blur' }
+        ],
+        // 商品重量的校验规则
+        goods_weight: [
+          { required: true, message: '请输入商品重量', triggger: 'blur' }
+        ],
+        // 商品所属分类的校验规则
+        goods_cat: [
+          { required: true, message: '请选择商品分类', triggger: 'blur' }
+        ]
+      },
+      // 获取的商品分类数据
+      cateList: [],
+      // 级联选择器的配置信息
+      cateProps: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children',
+        expandTrigger: 'hover'
+      }
     }
   },
   created () {
-
+    this.getCateList()
   },
   methods: {
+    /**
+     * 获取所有的商品分类数据
+     */
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories')
 
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类数据失败！')
+      }
+
+      this.cateList = res.data
+    },
+    /**
+     * 商品分类级联选择器选中项发生变化、会触发的函数
+     */
+    cateHandleChange () {
+      // 不是第三级分类的话、清空数据
+      if (this.addForm.goods_cat.length !== 3) {
+        this.addForm.goods_cat = []
+      }
+    }
   }
 }
 </script>
