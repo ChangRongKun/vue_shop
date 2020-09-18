d8adf51fab86027951acbe283e7c692adbd56d33
ChangRongKun <cccrk123@163.com>
2020-09-18 11:05:49 +0800

    实现省市区/县数据的联动效果

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -60,7 +60,8 @@
           <template>
             <el-button type="primary"
                        size="mini"
-                       icon="el-icon-edit"></el-button>
+                       icon="el-icon-edit"
+                       @click="showBox"></el-button>
             <el-button type="success"
                        size="mini"
                        icon="el-icon-location"></el-button>
@@ -82,10 +83,39 @@
 
     </el-card>
 
+    <!-- 修改地址的对话框 -->
+    <el-dialog title="修改地址"
+               :visible.sync="addressDialogVisible"
+               width="50%"
+               @close="addressDialogClosed">
+      <el-form :model="addressForm"
+               :rules="addressFormRules"
+               ref="addressFormRef"
+               label-width="100px">
+        <el-form-item label="省市区/县"
+                      prop="address1">
+          <el-cascader :options="cityData"
+                       v-model="addressForm.address1"></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址"
+                      prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button @click="addressDialogVisible = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="addressDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
+import cityData from './citydata.js'
 export default {
   data () {
     return {
@@ -98,7 +128,27 @@ export default {
       // 获取的订单列表数据
       orderList: [],
       // 总条数
-      total: 0
+      total: 0,
+      // 控制修改地址对话框的显示或隐藏
+      addressDialogVisible: false,
+      // 地址表单所需的数据
+      addressForm: {
+        // 省市区/县
+        address1: [],
+        // 详细地址
+        adderss2: ''
+      },
+      // 地址表单的验证规则
+      addressFormRules: {
+        address1: [
+          { required: true, message: '请选择省市区/县', trigger: 'blur' }
+        ],
+        address2: [
+          { required: true, message: '请选择详细地址', trigger: 'blur' }
+        ]
+      },
+      // 城市信息
+      cityData
     }
   },
   created () {
@@ -132,10 +182,25 @@ export default {
     handleCurrentChange (newPage) {
       this.queryInfo.pagenum = newPage
       this.getOrderList()
+    },
+    /**
+     * 展示修改地址的对话框
+     */
+    showBox () {
+      this.addressDialogVisible = true
+    },
+    /**
+     * 监听关闭修改地址的对话框
+     */
+    addressDialogClosed () {
+      this.$refs.addressFormRef.resetFields()
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.el-cascader {
+  width: 100%;
+}
 </style>
