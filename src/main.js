99421b107cf43bb1a6d0208cc61ad2f3e0431cad
ChangRongKun <cccrk123@163.com>
2020-09-18 15:26:35 +0800

    通过nprogress添加进度条效果

--- src/main.js
+++ src/main.js
@@ -14,14 +14,30 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// 导入 NProgress 包对应的 JS和 CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-// 设置请求拦截器、保证拥有获取数据的权限
+
+/**
+ * 设置请求拦截器、保证拥有获取数据的权限
+ * 在 request 拦截器中、展示进度条 NProgress.start()
+ */
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+// 在 response 拦截器中、隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
