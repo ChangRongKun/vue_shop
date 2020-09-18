091e3fe9043492c0734ae42f182f83b163df2f49
ChangRongKun <cccrk123@163.com>
2020-09-18 10:42:01 +0800

    获取订单列表数据、渲染table表格、并实现分页功能

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -20,6 +20,66 @@
         </el-col>
       </el-row>
 
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList"
+                border
+                stripe>
+        <!-- 索引列 -->
+        <el-table-column type="index"
+                         label="#"></el-table-column>
+
+        <el-table-column label="订单编号"
+                         prop="order_number"></el-table-column>
+
+        <el-table-column label="订单价格"
+                         prop="order_price"
+                         width="80px"></el-table-column>
+
+        <el-table-column label="是否付款"
+                         prop="pay_status"
+                         width="80px">
+          <template slot-scope="scope">
+            <el-tag type="success"
+                    v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger"
+                    v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="是否发货"
+                         prop="is_send"
+                         width="80px"></el-table-column>
+
+        <el-table-column label="下单时间">
+          <template slot-scope="scope">
+            {{scope.row.create_time | dateFormat}}
+          </template>
+        </el-table-column>
+
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary"
+                       size="mini"
+                       icon="el-icon-edit"></el-button>
+            <el-button type="success"
+                       size="mini"
+                       icon="el-icon-location"></el-button>
+          </template>
+        </el-table-column>
+
+      </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"
+                     :current-page="queryInfo.pagenum"
+                     :page-sizes="[5, 10, 15,20]"
+                     :page-size="queryInfo.pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="total"
+                     background>
+      </el-pagination>
+
     </el-card>
 
   </div>
@@ -29,14 +89,50 @@
 export default {
   data () {
     return {
-
+      // 查询订列表所需要的对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 获取的订单列表数据
+      orderList: [],
+      // 总条数
+      total: 0
     }
   },
   created () {
-
+    this.getOrderList()
   },
   methods: {
+    /**
+     * 获取订单列表
+     */
+    async getOrderList () {
+      const { data: res } = await this.$http.get('orders', { params: this.queryInfo })
 
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败！')
+      }
+
+      this.orderList = res.data.goods
+
+      this.total = res.data.total
+    },
+    /**
+     * pagesize 发生改变出发的事件
+     */
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    /**
+     * pagenum 发生改变触发的事件
+     */
+    handleCurrentChange (newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    }
   }
 }
 </script>
